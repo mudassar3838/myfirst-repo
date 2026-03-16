bd8a98ddb328ab18bd425cca9a9f6390223be9cf
mudassar3838 <mudassardogar38@gmail.com>
2026-03-16 11:49:16 +0500

    empty fie by developer2

--- dev1.py
+++
@@ -1,6 +0,0 @@
-<<<<<<< HEAD
-print("develpor 1 working on speciel project")
-=======
-print("develpor 1 working on speciel project")
-print("i have recieved your i am developer2")
->>>>>>> 401b05d (updated by developer 2)
