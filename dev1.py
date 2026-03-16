899a69dca7da164406003452672eb32dc5ebbe8f
mudassar3838 <mudassardogar38@gmail.com>
2026-03-16 22:00:52 +0500

    confilict fix

--- dev1.py
+++ dev1.py
@@ -1,6 +1,3 @@
 print("this is oroginaly from developer1 code")
-<<<<<<< HEAD
-=======
 print("modified by developer2")
 print("this addition commit by developer 2")
->>>>>>> 31a8f50 (developer2 add this line withou sync)
