59a1d2d2425c3853dac46e06cdaeca5f619601ff
mudassar3838 <mudassardogar38@gmail.com>
2026-03-16 15:48:16 +0500

    update dev1.py

--- dev1.py
+++ dev1.py
@@ -1,1 +1,6 @@
 print("this is oroginaly from developer1 code")
+<<<<<<< HEAD
+=======
+print("modified by developer2")
+print("this addition commit by developer 2")
+>>>>>>> 31a8f50 (developer2 add this line withou sync)
