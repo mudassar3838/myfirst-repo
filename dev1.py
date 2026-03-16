35d12661616ecbd1c97f1b6e3bb01dd8da10ab6d
mudassar3838 <mudassardogar38@gmail.com>
2026-03-16 11:28:08 +0500

    updated dev1.py

--- dev1.py
+++ dev1.py
@@ -1,1 +1,6 @@
+<<<<<<< HEAD
 print("develpor 1 working on speciel project")
+=======
+print("develpor 1 working on speciel project")
+print("i have recieved your i am developer2")
+>>>>>>> 401b05d (updated by developer 2)
